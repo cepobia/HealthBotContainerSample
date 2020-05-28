97659ada7d79faa1cdd582f8ef4adc67223e7bea
Esteban Londono <esteban.londono.padilla@gmail.com>
2020-05-28 11:46:16 -0500

    chake trigger

--- public/index.js
+++ public/index.js
@@ -107,7 +107,7 @@ function initBotConversation() {
                             // Use the following activity to proactively invoke a bot scenario
                             
                             triggeredScenario: {
-                                trigger: "triage",
+                                trigger: "covid19_assessment",
                                 /*
                                 args: {
                                     myVar1: "{custom_arg_1}",
